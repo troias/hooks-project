b67b6ecc1efe91e1d8247ae10e4dfa1c12b6ffab
Troy Flavell <hunter007nz@gmail.com>
2021-04-04 23:45:35 +1200

    implemented useCallback hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,  useCallback } from 'react';
 import IngredientList from './IngredientList'
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,22 +6,6 @@ import Search from './Search';
 const Ingredients = () => {
 
   const [ingredientsState, setIngredientsState] = useState([])
-
-  useEffect(() => {
-    fetch('https://react-hooks-update-af222-default-rtdb.firebaseio.com/ingredients.json')
-    .then(res => res.json())
-    .then(resData => {
-      const loadedIngredients = []
-      for (const key in resData) {
-        loadedIngredients.push({
-          id: key, 
-          title: resData[key].title, 
-          amount: resData[key].amount
-        })
-      }
-      setIngredientsState(loadedIngredients)
-    })
-  }, [])
 
   useEffect(()=> {
     console.log('[useEffect], rendering ingredient')
@@ -47,12 +31,16 @@ const Ingredients = () => {
 
   }
 
+  const onLoadIngredientsHandler = useCallback((ingredients) => {
+    setIngredientsState(ingredients)
+  }, [])
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={onLoadIngredientsHandler}/>
         <IngredientList
           ingredients={ingredientsState}
           onRemoveItem={() => { }}
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,43 @@
-import React from 'react';
-
+import React, { useState, useEffect, } from 'react';
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const { onLoadIngredients } = props
+  const [enteredFilter, setEnteredFilter] = useState('')
+
+  useEffect(() => {
+    const queryParams = enteredFilter.length === 0
+      ? ''
+      : `?orderBy="title"&equalTo="${enteredFilter}"`
+
+    fetch('https://react-hooks-update-af222-default-rtdb.firebaseio.com/ingredients.json' + queryParams)
+      .then(res =>
+        res.json())
+      .then(resData => {
+        const filteredIngredients = []
+        for (const key in resData) {
+          filteredIngredients.push({
+            id: key,
+            title: resData[key].title,
+            amount: resData[key].amount
+          })
+        }
+        props.onLoadIngredients(filteredIngredients)
+      })
+  }, [enteredFilter, onLoadIngredients])
+
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input type="text"
+            value={enteredFilter}
+            onChange={event => {
+              setEnteredFilter(event.target.value)
+            }} />
         </div>
       </Card>
     </section>
