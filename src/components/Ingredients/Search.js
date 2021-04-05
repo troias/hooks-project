797a6e93c713ae255fd8e2e6a5f4d95492ea8d8a
Troy Flavell <hunter007nz@gmail.com>
2021-04-05 14:48:52 +1200

    useRef

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,31 +1,37 @@
-import React, { useState, useEffect, } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+
   const { onLoadIngredients } = props
   const [enteredFilter, setEnteredFilter] = useState('')
+  const inputRef = useRef()
 
   useEffect(() => {
-    const queryParams = enteredFilter.length === 0
-      ? ''
-      : `?orderBy="title"&equalTo="${enteredFilter}"`
-
-    fetch('https://react-hooks-update-af222-default-rtdb.firebaseio.com/ingredients.json' + queryParams)
-      .then(res =>
-        res.json())
-      .then(resData => {
-        const filteredIngredients = []
-        for (const key in resData) {
-          filteredIngredients.push({
-            id: key,
-            title: resData[key].title,
-            amount: resData[key].amount
-          })
-        }
-        props.onLoadIngredients(filteredIngredients)
-      })
-  }, [enteredFilter, onLoadIngredients])
+    setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const queryParams = enteredFilter.length === 0
+        ? ''
+        : `?orderBy="title"&equalTo="${enteredFilter}"`
+  
+      fetch('https://react-hooks-update-af222-default-rtdb.firebaseio.com/ingredients.json' + queryParams)
+        .then(res =>
+          res.json())
+        .then(resData => {
+          const filteredIngredients = []
+          for (const key in resData) {
+            filteredIngredients.push({
+              id: key,
+              title: resData[key].title,
+              amount: resData[key].amount
+            })
+          }
+          props.onLoadIngredients(filteredIngredients)
+        })
+      }
+    }, 500)
+  }, [enteredFilter, onLoadIngredients, inputRef])
 
 
   return (
@@ -34,6 +40,7 @@ const Search = React.memo(props => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input type="text"
+            ref={inputRef}
             value={enteredFilter}
             onChange={event => {
               setEnteredFilter(event.target.value)
